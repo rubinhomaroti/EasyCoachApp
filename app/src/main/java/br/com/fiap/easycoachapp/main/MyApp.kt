11da62e14d7989d935815788aba091ddb738aa9e
Rubens Maroti F <rubens@mobilemed.com.br>
2021-07-05 19:59:34 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/easycoachapp/main/MyApp.kt
@@ -0,0 +1,10 @@
+package br.com.fiap.easycoachapp.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
