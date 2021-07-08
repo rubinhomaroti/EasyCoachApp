b52ea5d6927cc3fbfeaa072700f0a2c30e102edf
Rubens Maroti F <rubens@mobilemed.com.br>
2021-07-08 13:04:28 -0300

    Inicializa registro do usuário no Firestore após o signUp

--- app/src/main/java/br/com/fiap/easycoachapp/data/signUp/useCases/DoSignUp.kt
+++ app/src/main/java/br/com/fiap/easycoachapp/data/signUp/useCases/DoSignUp.kt
@@ -1,14 +1,21 @@
 package br.com.fiap.easycoachapp.data.signUp.useCases
 
+import br.com.fiap.easycoachapp.domain.entities.CoachEntity
+import br.com.fiap.easycoachapp.domain.entities.CoacheeEntity
 import br.com.fiap.easycoachapp.domain.entities.UserEntity
 import br.com.fiap.easycoachapp.domain.helpers.DomainError
 import br.com.fiap.easycoachapp.domain.helpers.UserType
+import br.com.fiap.easycoachapp.domain.helpers.UserType.*
 import br.com.fiap.easycoachapp.domain.usecases.signUp.DoSignUpContract
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 
-class DoSignUp (
-    private val auth: FirebaseAuth
+class DoSignUp(
+    private val auth: FirebaseAuth,
+    private val db: FirebaseFirestore
 ) : DoSignUpContract {
     override fun execute(
         user: UserEntity,
@@ -20,17 +27,60 @@ class DoSignUp (
             auth.createUserWithEmailAndPassword(user.email, user.password)
                 .addOnSuccessListener {
                     val currentUser = auth.currentUser
-                    val userProfileChangeRequest = UserProfileChangeRequest.Builder().setDisplayName(user.name).build()
+                    val userProfileChangeRequest =
+                        UserProfileChangeRequest.Builder().setDisplayName(user.name).build()
                     currentUser!!.updateProfile(userProfileChangeRequest)
-                        .addOnCompleteListener{
-                            onSuccessListener()
+                        .addOnCompleteListener {
+                            registerUserDetails(user, type).addOnCompleteListener {
+                                onSuccessListener()
+                            }
                         }
                 }
-                .addOnFailureListener{
+                .addOnFailureListener {
                     onFailureListener(DomainError.SIGN_UP_ERROR)
                 }
         } else {
             onFailureListener(DomainError.SIGN_UP_ERROR)
+        }
+    }
+
+    private fun registerUserDetails(user: UserEntity, type: UserType): Task<DocumentReference> {
+        val currentUser = auth.currentUser
+        when (type) {
+            COACH -> {
+                val coach = CoachEntity(
+                    uid = currentUser!!.uid,
+                    email = user.email,
+                    password = user.password,
+                    name = user.name,
+                    birthDate = user.birthDate,
+                    sex = user.sex,
+                    cpf = user.cpf,
+                    contactNumber = user.contactNumber,
+                    cancellationFee = 0.10F,
+                    cnpj = null,
+                    coachees = null,
+                    sessionPackages = null,
+                    sessions = null,
+                    specialties = null
+                )
+                return db.collection("coachs").add(coach)
+            }
+            COACHEE -> {
+                val coachee = CoacheeEntity(
+                    uid = currentUser!!.uid,
+                    email = user.email,
+                    password = user.password,
+                    name = user.name,
+                    birthDate = user.birthDate,
+                    sex = user.sex,
+                    cpf = user.cpf,
+                    contactNumber = user.contactNumber,
+                    sessions = null,
+                    coachs = null
+                )
+                return db.collection("coachees").add(coachee)
+            }
         }
     }
 }
