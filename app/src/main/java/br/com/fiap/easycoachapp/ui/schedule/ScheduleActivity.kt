ddb3e45d48752c678c10490ba57a04808809e1b3
Rubens Maroti F <rubens@mobilemed.com.br>
2021-07-22 19:30:05 -0300

    Cria primeira versão de tela de agenda

---
+++ app/src/main/java/br/com/fiap/easycoachapp/ui/schedule/ScheduleActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.easycoachapp.ui.schedule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.easycoachapp.R
+
+class ScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule)
+    }
+}
