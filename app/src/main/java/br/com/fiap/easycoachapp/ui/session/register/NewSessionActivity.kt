b2fc34645b505f2430e52ae00c71f5276ef11615
Rubens Maroti F <rubens@mobilemed.com.br>
2021-07-26 20:20:22 -0300

    Refatora activities

---
+++ app/src/main/java/br/com/fiap/easycoachapp/ui/session/register/NewSessionActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.easycoachapp.ui.session.register
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.fiap.easycoachapp.R
+
+class NewSessionActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_session)
+    }
+}
