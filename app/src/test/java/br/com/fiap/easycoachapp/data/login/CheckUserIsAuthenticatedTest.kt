5f3ccf81608faa26e889f9ce964391e774825033
Rubens Maroti F <rubens@mobilemed.com.br>
2021-07-30 09:27:40 -0300

    Cria teste do caso de uso CheckUserIsAuthenticated

---
+++ app/src/test/java/br/com/fiap/easycoachapp/data/login/CheckUserIsAuthenticatedTest.kt
@@ -0,0 +1,45 @@
+package br.com.fiap.easycoachapp.data.login
+
+import br.com.fiap.easycoachapp.data.login.useCases.CheckUserIsAuthenticated
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
+class CheckUserIsAuthenticatedTest {
+    var sut: CheckUserIsAuthenticated? = null
+    private val firebaseAuth: FirebaseAuth = Mockito.mock(FirebaseAuth::class.java)
+    private val firebaseUser: FirebaseUser = Mockito.mock(FirebaseUser::class.java)
+
+    private fun mockFirebaseAuthSuccess() {
+        Mockito.`when`(firebaseAuth.currentUser).thenAnswer {
+            return@thenAnswer firebaseUser
+        }
+    }
+
+    private fun mockFirebaseAuthFailure() {
+        Mockito.`when`(firebaseAuth.currentUser).thenAnswer {
+            return@thenAnswer null
+        }
+    }
+
+    @Before
+    fun setup() {
+        sut = CheckUserIsAuthenticated(firebaseAuth)
+        mockFirebaseAuthSuccess()
+    }
+
+    @Test
+    fun `Should return true is firebase auth user is not null`() {
+        val result = sut?.execute()
+        assert(result == true)
+    }
+    @Test
+    fun `Should return false is firebase auth user is null`() {
+        mockFirebaseAuthFailure()
+
+        val result = sut?.execute()
+        assert(result == false)
+    }
+}
